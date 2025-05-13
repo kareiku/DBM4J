2c5fc4ab4d84e7b1c1aa9daa4e7790e0e22142ca
kareiku <xaxvu7@gmail.com>
2025-05-13 16:04:22 +0200

    refactor: queries are now pairs (fmt, args) and urls are now set in the constructor

--- src/main/java/io/github/kareiku/DatabaseManager.java
+++ src/main/java/io/github/kareiku/DatabaseManager.java
@@ -1,6 +1,7 @@
 package io.github.kareiku;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -8,19 +9,13 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class DatabaseManager implements IDatabaseManager {
-    private String url;
+    private final String url;
 
-    public DatabaseManager() {
-        this.url = null;
-    }
-
-    @Override
-    public void setUrl(@NotNull String url) {
+    public DatabaseManager(@NotNull String url) {
         this.url = url;
     }
 
-    @Override
-    public void setUrl(@NotNull String subprotocol, @NotNull String subname) {
+    public DatabaseManager(@NotNull String subprotocol, @NotNull String subname) {
         this.url = String.format("jdbc:%s:%s", subprotocol, subname);
     }
 
@@ -29,22 +24,22 @@ public class DatabaseManager implements IDatabaseManager {
     }
 
     @Override
-    public void update(@NotNull String query) throws SQLException {
+    public void update(@NotNull String fmt, @NotNull Object @Nullable ... args) throws SQLException {
         try (
                 Connection connection = this.getConnection();
                 Statement statement = connection.createStatement()
         ) {
-            statement.executeUpdate(query);
+            statement.executeUpdate(String.format(fmt, args));
         }
     }
 
     @Override
-    public @NotNull Stream<Stream<?>> fetch(@NotNull String query) throws SQLException {
+    public @NotNull Stream<@NotNull Stream<?>> fetch(@NotNull String fmt, @NotNull Object @Nullable ... args) throws SQLException {
         List<List<?>> table = new ArrayList<>();
         try (
                 Connection connection = this.getConnection();
                 Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery(query)
+                ResultSet resultSet = statement.executeQuery(String.format(fmt, args))
         ) {
             int columnCount = resultSet.getMetaData().getColumnCount();
             while (resultSet.next()) {
