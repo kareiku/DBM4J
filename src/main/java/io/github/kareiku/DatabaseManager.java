461411c5b1ff00a3401eb663fe8698300c684a43
kareiku <xaxvu7@gmail.com>
2025-05-26 04:08:52 +0200

    Created an interface to represent evety CRUD-based database manager

--- src/main/java/io/github/kareiku/DatabaseManager.java
+++ src/main/java/io/github/kareiku/DatabaseManager.java
@@ -1,5 +1,6 @@
 package io.github.kareiku;
 
+<<<<<<< HEAD
 import java.sql.SQLException;
 import java.util.stream.Stream;
 
@@ -7,4 +8,14 @@ public interface IDatabaseManager {
     void update(String fmt, Object... args) throws SQLException;
 
     Stream<Stream<?>> fetch(String fmt, Object... args) throws SQLException;
+=======
+public interface DatabaseManager {
+    void create(String query, Object... more);
+
+    String read(String query, Object... more);
+
+    void update(String query, Object... more);
+
+    void delete(String query, Object... more);
+>>>>>>> 1448cd1 (Created an interface to represent evety CRUD-based database manager)
 }
