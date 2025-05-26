3a9914f797fb30398f31904d15a21d549f0f45c7
kareiku <xaxvu7@gmail.com>
2025-05-26 04:26:32 +0200

    Remade the SQL-based database manager to follow the CRUD interface

--- src/main/java/io/github/kareiku/SQLDatabaseManager.java
+++ src/main/java/io/github/kareiku/SQLDatabaseManager.java
@@ -1,12 +1,12 @@
 package io.github.kareiku;
 
+<<<<<<< HEAD
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+=======
+>>>>>>> ae2178c (Remade the SQL-based database manager to follow the CRUD interface)
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
 
 public class DatabaseManager implements IDatabaseManager {
     private final String url;
@@ -24,44 +24,84 @@ public class DatabaseManager implements IDatabaseManager {
     }
 
     @Override
+<<<<<<< HEAD
     public void update(@NotNull String fmt, @NotNull Object @Nullable ... args) throws SQLException {
         try (
                 Connection connection = this.getConnection();
                 PreparedStatement statement = connection.prepareStatement(fmt)
+=======
+    public void create(String query, Object... args) {
+        this.update(query, args);
+    }
+
+    @Override
+    public String read(String query, Object... args) {
+        try (
+                Connection connection = DriverManager.getConnection(this.url);
+                PreparedStatement statement = connection.prepareStatement(query)
+>>>>>>> ae2178c (Remade the SQL-based database manager to follow the CRUD interface)
         ) {
-            if (args != null) {
-                for (int i = 0; i < args.length; i++) {
-                    statement.setObject(i + 1, args[i]);
-                }
+            for (int i = 0; i < args.length; i++) {
+                statement.setObject(i + 1, args[i]);
             }
-            statement.executeUpdate();
+            try (ResultSet resultSet = statement.executeQuery()) {
+                ResultSetMetaData meta = resultSet.getMetaData();
+                int columnCount = meta.getColumnCount();
+                StringBuilder json = new StringBuilder();
+                json.append('[');
+                boolean firstRow = true;
+                while (resultSet.next()) {
+                    if (!firstRow) json.append(',');
+                    json.append('{');
+                    for (int i = 1; i <= columnCount; i++) {
+                        json.append('"').append(meta.getColumnName(i)).append('"').append(':');
+                        Object value = resultSet.getObject(i);
+                        if (value == null) {
+                            json.append("null");
+                        } else if (value instanceof Number || value instanceof Boolean) {
+                            json.append(value);
+                        } else {
+                            json.append('"').append(value.toString().replace("\"", "\\\"")).append('"');
+                        }
+                        if (i < columnCount) json.append(',');
+                    }
+                    json.append('}');
+                    firstRow = false;
+                }
+                json.append(']');
+                return json.toString();
+            }
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+            return "{}";
         }
     }
 
     @Override
+<<<<<<< HEAD
     public @NotNull Stream<@NotNull Stream<?>> fetch(@NotNull String fmt, @NotNull Object @Nullable ... args) throws SQLException {
         List<List<?>> table = new ArrayList<>();
         try (
                 Connection connection = this.getConnection();
                 PreparedStatement statement = connection.prepareStatement(fmt)
+=======
+    public void update(String query, Object... args) {
+        try (
+                Connection connection = DriverManager.getConnection(this.url);
+                PreparedStatement statement = connection.prepareStatement(query)
+>>>>>>> ae2178c (Remade the SQL-based database manager to follow the CRUD interface)
         ) {
-            if (args != null) {
-                for (int i = 0; i < args.length; i++) {
-                    statement.setObject(i + 1, args[i]);
-                }
+            for (int i = 0; i < args.length; i++) {
+                statement.setObject(i + 1, args[i]);
             }
-
-            try (ResultSet resultSet = statement.executeQuery()) {
-                int columnCount = resultSet.getMetaData().getColumnCount();
-                while (resultSet.next()) {
-                    List<Object> record = new ArrayList<>();
-                    table.add(record);
-                    for (int i = 1; i <= columnCount; i++) {
-                        record.add(resultSet.getObject(i));
-                    }
-                }
-            }
+            statement.executeUpdate();
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
         }
-        return table.stream().map(List::stream);
+    }
+
+    @Override
+    public void delete(String query, Object... args) {
+        this.update(query, args);
     }
 }
